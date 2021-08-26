bc38a264ca7519f6e0198311c8916969ba001e0e
Ty Everton <everton_915@yahoo.com>
2021-08-26 11:39:47 -0600

    nearly complete with creating.mjs file

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,16 +1,66 @@
-import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
+import setText, { appendText, showWaiting, hideWaiting } from './results.mjs'
 
 export function get() {
+  axios.get('http://localhost:3000/orders/1').then(({ data }) => {
+    setText(JSON.stringify(data))
+  })
 }
 
+//the promise above starts by calling an async function, then chains the then function following the get function. the "then" function grabs the data from the get function and sets it as a human readable format
+
 export function getCatch() {
+  axios
+    .get('http://localhost:3000/orders/123')
+    .then(({ data }) => {
+      setText(JSON.stringify(data))
+    })
+    .catch((err) => setText(err))
 }
 
 export function chain() {
+  axios
+    .get('http://localhost:3000/orders/1')
+    .then(({ data }) => {
+      return axios.get(
+        `http://localhost:3000/addresses/${data.shippingAddress}`,
+      )
+    })
+    .then(({ data }) => {
+      setText(`City: ${data.city}`)
+    })
 }
-
+//lines 20 - 31 - 21/22 take an order, then *lines 23-25* it takes that order and reads the address,  then lines 28-29 display the address
 export function chainCatch() {
+  axios
+    .get('http://localhost:3000/orders/1')
+    .then(({ data }) => {
+      return axios.get(
+        `http://localhost:3000/addresses/${data.shippingAddress}`,
+      )
+    })
+    .then(({ data }) => {
+      setText(`City: ${data.my.city}`)
+    })
+    .catch((err) => setText(err))
 }
 
 export function final() {
+  showWaiting()
+  axios
+    .get('http://localhost:3000/orders/1')
+    .then(({ data }) => {
+      return axios.get(
+        `http://localhost:3000/addresses/${data.shippingAddress}`,
+      )
+    })
+    .then(({ data }) => {
+      setText(`City: ${data.city}`)
+    })
+    .catch((err) => setText(err))
+    .finally(() => {
+      setTimeout(() => {
+        hideWaiting()
+      }, 1500)
+      appendText(' -- Completed --')
+    })
 }
--- src/creating.mjs
+++ src/creating.mjs
@@ -1,22 +1,44 @@
-import setText, { appendText } from "./results.mjs";
+import setText, { appendText } from './results.mjs'
 
-export function timeout(){
+export function timeout() {
+  const wait = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Timeout!')
+    }, 1500)
+  })
+
+  wait.then((text) => setText(text))
 }
 
-export function interval(){
+export function interval() {
+  let counter = 0
+  const wait = new Promise((resolve) => {
+    setInterval(() => {
+      console.log('INTERVAL')
+      resolve(`Timeout! ${++counter}`)
+    }, 1500)
+  })
+  wait
+    .then((text) => setText(text))
+    .finally(() => appendText(`--Done ${counter}`))
 }
 
-export function clearIntervalChain(){
+export function clearIntervalChain() {
+  let counter = 0
+  let interval
+  const wait = new Promise((resolve) => {
+    interval = setInterval(() => {
+      console.log('INTERVAL')
+      resolve(`Timeout! ${++counter}`)
+    }, 1500)
+  })
+  wait.then((text) => setText(text)).finally(() => clearInterval(interval))
 }
 
-export function xhr(){
-}
+export function xhr() {}
 
-export function allPromises(){
-}
+export function allPromises() {}
 
-export function allSettled(){
-}
+export function allSettled() {}
 
-export function race(){
-}
+export function race() {}
